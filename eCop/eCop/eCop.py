0454c6a42a11a981545a9e18f3f62cb3f4722c68
Paul Clay <paul_clay@hotmail.com>
2016-03-04 22:35:53 +1100

    added comments

--- eCop/eCop/eCop.py
+++ eCop/eCop/eCop.py
@@ -1,19 +1,19 @@
 #file = open("Test1.txt")
-file = open("Test2.txt")
+file = open("Test2.txt")                                                                    # open input file
 
-limit, signal_fine, slow_fine, fast_fine = map(int, file.readline().strip().split())          # skip first line of input file
+limit, signal_fine, slow_fine, fast_fine = map(int, file.readline().strip().split())        # read first line as integers
 
-for line in file:
-    plate, speed, signal = line.strip().split()
-    speed = int(speed)
-    fine = 0
-    if signal == "red":
+for line in file:                                                                           # for each remaing line in the file
+    plate, speed, signal = line.strip().split()                                             # read data into variables
+    speed = int(speed)                                                                      # convert speed to integer
+    fine = 0                                                                                # initialise fine
+    if signal == "red":                                                                     # add fine for red signal
         fine = fine + signal_fine
-    if speed - limit > 15:
+    if speed - limit > 15:                                                                  # add fine for exceeding speed limit by more than 15 km/h
         fine = fine + fast_fine
-    elif speed - limit >= 5:
+    elif speed - limit >= 5:                                                                # add fine for exceeding speed limit by at least 5 km/h
         fine = fine + slow_fine
 
-    if fine > 0:
+    if fine > 0:                                                                            # print plate and fine if there is a fine
         print('{:6} ${}'.format(plate, fine))
     
